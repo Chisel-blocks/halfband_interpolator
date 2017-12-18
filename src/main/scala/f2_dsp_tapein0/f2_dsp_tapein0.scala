099d0893edf7b556770afa80bd6e357d3ace5bec
Marko Kosunen <marko.kosunen@aalto.fi>
2017-12-18 09:53:26 -0800

    F2_dsp_tapein0 scala now buffers complex numbers

--- src/main/scala/f2_dsp_tapein0/f2_dsp_tapein0.scala
+++ src/main/scala/f2_dsp_tapein0/f2_dsp_tapein0.scala
@@ -4,19 +4,26 @@
 //Start with a static tb and try to genererate a gnerator for it
 package f2_dsp_tapein0
 
+import chisel3.experimental._
 import chisel3._
-import java.io.{File, FileWriter, BufferedWriter}
 
 class f2_dsp_tapein0 extends Module {
   val io = IO(new Bundle {
-    val iptr_A        = Input(UInt(16.W))
-    val _Z             = Output(UInt(16.W))
+    val clock_DSP     = Input(Clock())
+    val iptr_Areal    = Input(UInt(16.W))
+    val iptr_Aimag    = Input(UInt(16.W))
+    val Zreal         = Output(UInt(16.W))
+    val Zimag         = Output(UInt(16.W))
   })
 
-  val x  = Reg(UInt())
+  val inregreal  = RegNext(io.iptr_Areal)
+  val inregimag  = RegNext(io.iptr_Aimag)
 
-  x := io.iptr_A
-  io._Z := x
+  withClock (io.clock_DSP){
+    io.Zreal := RegNext(inregreal)
+    io.Zimag := RegNext(inregimag)
+  }
+
 }
 
 //This gives you verilog
