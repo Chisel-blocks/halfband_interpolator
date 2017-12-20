6ab13aff6e1b2669fe3550454c507bb9072ad8ea
Marko Kosunen <marko.kosunen@aalto.fi>
2017-12-20 13:46:02 -0800

    Tb generator for f2_dsp_tapein0 with handlebars

--- verilog/tb_f2_dsp_tapein0.v
+++ verilog/tb_f2_dsp_tapein0.v
@@ -1,65 +1,75 @@
 //This is a tesbench generated with scala generator
 //Things you want to control from the simulator cmdline must be parameters
-module tb_inverter #( parameter g_infile  = "./A.txt",
+module tb_f2_dsp_tapein0 #( parameter g_infile  = "./A.txt",
                       parameter g_outfile = "./Z.txt",
                       parameter g_Rs      = 160.0e6,
-                      parameter g_RS_DSP  = 20e6
+                      parameter g_Rs_DSP  = 20e6
                       );
 //timescale 1ps this should probably be a global model parameter 
-parameter c_Ts=1/(g_Rs*1e-12);
-parameter c_ratio=g_Rs/g_Rs_DSP
-parameter RESET_TIME = 5*c_Ts
-reg signed [15:0] io_iptr_Areal = 0;
-reg signed [15:0] io_iptr_Aimag = 0;
+parameter integer c_Ts=1/(g_Rs*1e-12);
+parameter integer c_ratio=g_Rs/(2.0*g_Rs_DSP);
+parameter RESET_TIME = 5*c_Ts;
+reg signed [15:0] io_iptr_A_real = 0;
+reg signed [15:0] io_iptr_A_imag = 0;
 reg io__Z;
 reg clock;
-reg reset;
 reg io_clock_DSP;
-wire reset;
-wire Z;
-integer StatusI, StatusO, infile, outfile;
-integer count
+reg reset;
+wire signed [15:0] io_Z_real;
+wire signed [15:0] io_Z_imag;
 
+integer StatusI, StatusO, infile, outfile;
+integer count;
+integer din1,din2;
+
+initial count = 0;
 initial clock = 1'b0;
-initial io_clk_DSP = 1'b0;
-initial reg = 1'b0;
+initial io_clock_DSP = 1'b0;
+initial reset = 1'b0;
 initial outfile = $fopen(g_outfile,"w"); // For writing
 always #(c_Ts)clock = !clock ;
 always @(posedge clock) begin 
-    if (count % c_ratio ==0) begin
+    if (count%c_ratio == 0) begin
         io_clock_DSP =! io_clock_DSP;
     end 
-    count++
+    count++;
 end
 
-always @(posedge io_clock_dsp) begin 
-    //if (count % c_ratio ==0) begin
-        $fwrite(fileOutID, "%d\t%d\n", io_dout_real, io_dout_imag);
-    //end 
+always @(posedge io_clock_DSP) begin 
+    //Print only valid values 
+    if (~($isunknown( io_Z_real)) &&   ~($isunknown( io_Z_imag))) begin
+        $fwrite(outfile, "%d\t%d\n", io_Z_real, io_Z_imag);
+    end
+    else begin
+        $fwrite(outfile, "%d\t%d\n", 0, 0);
+    end 
 end
 
-
-
-//tb_f2_dsp_tapein0 DUT(.clock(clock), 
-//.reset(reset),.io_clock_DSP(io_clock_DSP),
-// .A(iptr_A), .Z(Z) );
-
+f2_dsp_tapein0 DUT( // @[:@3.2]
+.clock(clock), // @[:@4.4]
+.reset(reset), // @[:@5.4]
+.io_clock_DSP(io_clock_DSP), // @[:@6.4]
+.io_iptr_A_real(io_iptr_A_real), // @[:@6.4]
+.io_iptr_A_imag(io_iptr_A_imag), // @[:@6.4]
+.io_Z_real(io_Z_real), // @[:@6.4]
+.io_Z_imag(io_Z_imag) // @[:@6.4]
+);
 
 initial #0 begin
-    reset=1
+    reset=1;
     #RESET_TIME
-    reset=0
+    reset=0;
     
     infile = $fopen(g_infile,"r"); // For reading
     while (!$feof(infile)) begin
 
             @(posedge clock) 
-             StatusI=$fscanf(fileInID, "%d\t%d\n", din1, din2);
-             io_iptr_Areal <= din1;
-             io_iptr_Aimag <= din2
+             StatusI=$fscanf(infile, "%d\t%d\n", din1, din2);
+             io_iptr_A_real <= din1;
+             io_iptr_A_imag <= din2;
     end
-    $fclose(fileInID);
-    $fclose(fileOutID);
+    $fclose(infile);
+    $fclose(outfile);
     $finish;
 end
 endmodule
