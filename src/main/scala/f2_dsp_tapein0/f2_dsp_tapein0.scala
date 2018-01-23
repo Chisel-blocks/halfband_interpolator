5d86c95962b566c81f6af787b4ce77349ba22078
Marko Kosunen <marko.kosunen@aalto.fi>
2018-01-22 17:38:35 -0800

    f2_dsp_tapein0 now supports multiple outputs

--- src/main/scala/f2_dsp_tapein0/f2_dsp_tapein0.scala
+++ src/main/scala/f2_dsp_tapein0/f2_dsp_tapein0.scala
@@ -10,20 +10,24 @@ import dsptools.numbers._
 import f2_decimator._
 
 
-class f2_dsp_io(n: Int=16) extends Bundle {
+class f2_dsp_io(n: Int=16, users: Int=2 ) extends Bundle {
     val decimator_controls = new f2_decimator_controls(gainbits=10)
     val iptr_A      = Input(DspComplex(SInt(n.W), SInt(n.W)))
-    val Z           = Output(DspComplex(SInt(n.W), SInt(n.W)))
+    val Z           = Output(Vec(users,DspComplex(SInt(n.W), SInt(n.W))))
+
 }
 
-class f2_dsp_tapein0 (n: Int=16) extends Module {
-  val io = IO( new f2_dsp_io(n=n)
+class f2_dsp_tapein0 (n: Int=16, users: Int=2) extends Module {
+  val io = IO( new f2_dsp_io(n=n,users=users)
   )
 
   val decimator  = Module ( new  f2_decimator (n=16, resolution=32, coeffres=16, gainbits=10))
   io.decimator_controls<>decimator.io.controls
   decimator.io.iptr_A:=io.iptr_A
-  io.Z:=decimator.io.Z
+  
+  for ( i <- 0 to users-1 ) { 
+      io.Z(i):=decimator.io.Z
+  }
 }
 
 //This gives you verilog
