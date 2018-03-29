3060cd0450e97d1a10d1c1c71565cb54dee8564d
Marko Kosunen <marko.kosunen@aalto.fi>
2018-03-28 18:02:48 -0700

    Input fifo added to f2_dsp_tapein5

--- src/main/scala/f2_dsp_tapein5/f2_dsp_tapein5.scala
+++ src/main/scala/f2_dsp_tapein5/f2_dsp_tapein5.scala
@@ -11,7 +11,7 @@ import freechips.rocketchip.util._
 import f2_decimator._
 import f2_rx_path_tapein5._
 
-class f2_dsp_io(n: Int=16, antennas: Int=4, users: Int=4 ) extends Bundle {
+class f2_dsp_io(n: Int=16, antennas: Int=4, users: Int=4) extends Bundle {
     val decimator_controls = Vec(antennas,new f2_decimator_controls(gainbits=10))    
     val iptr_A             = Input(Vec(antennas,DspComplex(SInt(n.W), SInt(n.W))))
     val user_index         = Input(UInt(log2Ceil(users).W)) //W should be log2 of users
@@ -19,37 +19,92 @@ class f2_dsp_io(n: Int=16, antennas: Int=4, users: Int=4 ) extends Bundle {
     val clock_symrate      = Input(Clock())
     val clock_symratex4    = Input(Clock())
     val clock_outfifo_deq  = Input(Clock())
+    val clock_infifo_enq   = Input(Clock())
     val reset_index_count  = Input(Bool())
     val reset_outfifo      = Input(Bool())
-    val output_mode        = Input(UInt(3.W))
+    val reset_infifo       = Input(Bool())
+    val rx_output_mode     = Input(UInt(3.W))
+    val input_mode         = Input(UInt(3.W))
     // Output bundle structure is determined by the sink (serdes), not by parameters
     //val Z                  =  Output(Vec(4,DspComplex(SInt(n.W), SInt(n.W))))
     val Z                  =  DecoupledIO(UInt((4*2*n+2).W))
+    val iptr_fifo          =  Flipped(DecoupledIO(UInt((4*2*n+2).W)))
     // Index to be transmitted. Indicates user, or antenna
     //val index              =  Output(UInt(2.W))
     override def cloneType = (new f2_dsp_io(n,antennas,users)).asInstanceOf[this.type]
 }
 
-class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
+class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4, fifodepth: Int=128 ) extends Module {
     val io = IO( new f2_dsp_io(n=n,users=users)
     )
   
     val rx_path  = VecInit(Seq.fill(antennas){ Module ( new  f2_rx_path_tapein5 (n=n, users=users)).io })
 
-    //Input assignments        
-    for ( i <- 0 to antennas-1){ 
+    //RX input assignments        
+    for ( i <-0 to antennas-1){ 
         io.decimator_controls(i)<>rx_path(i).decimator_controls
         rx_path(i).iptr_A:=io.iptr_A(i)
     }
 
-    //This is a 2 dimensional array, later to beconcatenated to a bitvector
+    //Input fifo from serdes
+    //Reformulate to RX compliant format.
+    val iproto=UInt((4*2*n+2).W)
+    val infifo = Module(new AsyncQueue(iproto,depth=fifodepth)).io
+    val r_iptr_fifo=  withClock(io.clock_symrate)(RegInit(VecInit(Seq.fill(4)(DspComplex.wire(0.S(n.W), 0.S(n.W))))))
+    val r_iptr_index= withClock(io.clock_symrate)(RegInit(0.U(2.W))) //For sake of symmetry,dunno if needed
+    val zero :: userssum :: Nil = Enum(2)
+    val inputmode=RegInit(zero)
+    
+    infifo.deq_reset:=io.reset_infifo
+    infifo.enq_reset:=io.reset_infifo
+    infifo.enq_clock:=io.clock_infifo_enq
+
+    infifo.enq<>io.iptr_fifo
+    infifo.deq_clock :=io.clock_symrate
+    when (io.input_mode===0.U) {
+        inputmode := zero
+        infifo.deq.ready:=false.B
+    } .elsewhen (io.input_mode===1.U ) {
+        inputmode := userssum
+        infifo.deq.ready:=true.B
+    } .otherwise {
+        inputmode:=zero
+        infifo.deq.ready:=false.B
+    }
+
+    when ( (infifo.deq.valid) && (inputmode===userssum)) { 
+        r_iptr_index := infifo.deq.bits(129,128)
+        for ( i <-0 to 3){
+            r_iptr_fifo(i).real:=infifo.deq.bits(i*2*n+n-1,i*2*n).asSInt
+            r_iptr_fifo(i).imag:=infifo.deq.bits(i*2*n+2*n-1,i*2*n+n).asSInt
+        }
+    } .elsewhen ( inputmode===zero ) {
+        r_iptr_index := 0.U
+        for ( i <-0 to 3){
+            r_iptr_fifo(i).real:=0.S
+            r_iptr_fifo(i).imag:=0.S
+        }
+    } .otherwise {
+        r_iptr_index := 0.U
+        for ( i <-0 to 3){
+            r_iptr_fifo(i).real:=0.S
+            r_iptr_fifo(i).imag:=0.S
+        
+        }
+    }
+
+    //This is a 4 element receiver array, later to be concatenated to a bitvector
     val w_Z=  Wire(Vec(4,DspComplex(SInt(n.W), SInt(n.W))))
     val w_index=  Wire(UInt(2.W))
    
     // First we generate all possible output signals, then we just select The one we want.
     //Generate the sum of users
     val sumusersstream = withClock(io.clock_symrate)(RegInit(VecInit(Seq.fill(users)(DspComplex.wire(0.S(n.W), 0.S(n.W))))))
-    
+    for (user <-0 to users-1){ 
+        sumusersstream(user):=rx_path.map( rxpath=> rxpath.Z(user)).foldRight(r_iptr_fifo(user))((usrleft,usrright)=> usrleft+usrright)
+    }
+  
+  
     //All antennas, single usere
     val seluser = withClock(io.clock_symrate)(RegInit(VecInit(Seq.fill(antennas)(DspComplex.wire(0.S(n.W), 0.S(n.W))))))
     for (antenna <-0 to antennas-1){ 
@@ -98,38 +153,36 @@ class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
     val mode=RegInit(bypass)
     
     //Decoder for the modes
-    when(io.output_mode===0.U){
+    when(io.rx_output_mode===0.U){
         mode := bypass
-    } .elsewhen(io.output_mode===1.U) {
+    } .elsewhen(io.rx_output_mode===1.U) {
         mode := select_users
-    } .elsewhen(io.output_mode===2.U) {
+    } .elsewhen(io.rx_output_mode===2.U) {
         mode:=select_antennas
-    } .elsewhen(io.output_mode===3.U) {
+    } .elsewhen(io.rx_output_mode===3.U) {
         mode:=select_both
-    } .elsewhen(io.output_mode===4.U) {
+    } .elsewhen(io.rx_output_mode===4.U) {
         mode:=stream_users
-    } .elsewhen(io.output_mode===5.U) {
+    } .elsewhen(io.rx_output_mode===5.U) {
         mode:=stream_rx
-    } .elsewhen(io.output_mode===6.U) {
+    } .elsewhen(io.rx_output_mode===6.U) {
         mode:=stream_sum
     }.otherwise {
         mode := bypass
     }
 
     // Fifo for ther output
-    val width=4*2*n+2
-    val depth=128
-    val proto=UInt(width.W)
-    val outfifo = Module(new AsyncQueue(proto,depth=depth))
+    val proto=UInt((4*2*n+2).W)
+    val outfifo = Module(new AsyncQueue(proto,depth=fifodepth)).io
 
     //Defaults
-    outfifo.io.enq_reset:=io.reset_outfifo 
-    outfifo.io.deq_reset:=io.reset_outfifo
-    outfifo.io.deq.ready:=io.Z.ready
-    outfifo.io.deq_clock:=io.clock_outfifo_deq
-    outfifo.io.enq_clock:=io.clock_symratex4
+    outfifo.enq_reset:=io.reset_outfifo 
+    outfifo.deq_reset:=io.reset_outfifo
+    outfifo.deq.ready:=io.Z.ready
+    outfifo.deq_clock:=io.clock_outfifo_deq
+    outfifo.enq_clock:=io.clock_symratex4
     w_index := withClock(io.clock_symrate)(RegNext(0.U))
-    io.Z.valid   := outfifo.io.deq.valid
+    io.Z.valid   := outfifo.deq.valid
     //Put something out if nothig else defined
     for (i <- 0 to 3) {
         w_Z(i) :=rx_path(i).Z(0)
@@ -139,7 +192,7 @@ class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
     val validcount  = withClock(io.clock_symratex4)(RegInit(0.U(2.W)))
     val validreg =  withClock(io.clock_symratex4)(RegInit(false.B))
     //control the valid signaö for the interface
-    when ( (mode===bypass) ||  (mode===select_users) ||  (mode===select_antennas) || (mode===select_both)  ) {
+    when ( (mode===bypass) ||  (mode===select_users) ||  (mode===select_antennas) || (mode===select_both) || (mode===stream_sum)  ) {
         // In these modes, the write rate is symrate
         when (validcount===3.U) {
             validcount:=0.U
@@ -156,13 +209,13 @@ class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
         validcount := 0.U
         validreg := false.B
     }
-    outfifo.io.enq.valid :=  validreg   
+    outfifo.enq.valid :=  validreg   
 
 
     //Mode operation definitions
     switch(mode) {
         is(bypass) {
-            when ( outfifo.io.enq.ready ){
+            when ( outfifo.enq.ready ){
                 for (i <- 0 to 3) {
                     w_Z(i) :=rx_path(i).Z(0)
                 }
@@ -170,7 +223,7 @@ class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
             }
         }
         is(select_users) {
-            when ( outfifo.io.enq.ready ){
+            when ( outfifo.enq.ready ){
                 for (i <- 0 to 3) {
                     w_Z(i) :=seluser(i)
                 }
@@ -178,14 +231,15 @@ class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
             }
         }
         is(select_antennas) {
-            when ( outfifo.io.enq.ready ){
+            when ( outfifo.enq.ready ){
                 for (i <- 0 to 3) {
                     w_Z(i) :=selrx(i)
                 }
+                w_index := withClock(io.clock_symrate)(RegNext(io.antenna_index))
             }
         }
         is(select_both) {
-            when ( outfifo.io.enq.ready ){
+            when ( outfifo.enq.ready ){
                 w_Z(0) :=selrxuser
                 w_Z(1) := DspComplex.wire(0.S,0.S)
                 w_Z(2) := DspComplex.wire(0.S,0.S)
@@ -195,31 +249,38 @@ class f2_dsp_tapein5 (n: Int=16, antennas: Int=4, users: Int=4) extends Module {
 
         }
         is(stream_users) {
-            when ( outfifo.io.enq.ready ){
+            when ( outfifo.enq.ready ){
                 w_Z := indexeduserstream    
                 w_index := withClock(io.clock_symratex4)(RegNext(index))
             }
 
         }
         is(stream_rx) {
-            when ( outfifo.io.enq.ready ){
+            when ( outfifo.enq.ready ){
                 w_Z := indexedrxstream    
                 w_index := withClock(io.clock_symratex4)(RegNext(index))
             }
         }
+        is(stream_sum) {
+            when ( outfifo.enq.ready ){
+                w_Z := sumusersstream    
+                w_index := withClock(io.clock_symrate)(RegNext(0.U))
+            }
+        }
     }
+    
+    //Here we reformat the output signals to a single bitvector
     val w_concat_Z=  Wire(UInt((4*2*n).W))
     val w_concat_Z_and_index=  Wire(UInt((4*2*n+2).W))
     w_concat_Z:=w_Z.map(x => Cat(x.imag,x.real).asUInt).reduceRight((msb,lsb)=>Cat(msb,lsb).asUInt)
     w_concat_Z_and_index:=Cat(w_index,w_concat_Z).asUInt
-    outfifo.io.enq.bits:=w_concat_Z_and_index
-    
+    outfifo.enq.bits:=w_concat_Z_and_index
+    io.Z.bits := outfifo.deq.bits
 
-    io.Z.bits := outfifo.io.deq.bits
 }
 
 //This gives you verilog
 object f2_dsp_tapein5 extends App {
-  chisel3.Driver.execute(args, () => new f2_dsp_tapein5)
+  chisel3.Driver.execute(args, () => new f2_dsp_tapein5(n=16, antennas=4, users=4, fifodepth=128 ))
 }
 
